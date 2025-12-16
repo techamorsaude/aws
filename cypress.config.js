1161220fd4aeb56635fd65ce28d322e0e5f031f2
ivan29071985 <ivan.santos@amorsaude.com>
2025-12-16 17:07:26 -0300

    Demonstração dos modulos para o Brenno

--- cypress.config.js
+++ cypress.config.js
@@ -18,4 +18,9 @@ module.exports = defineConfig({
     supportFile: 'cypress/support/e2e.js',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
+  env: {
+    allure: true,
+    allureResultsPath: 'allure-results',
+    allureReportLanguage: 'pt-BR',
+  },
 });
